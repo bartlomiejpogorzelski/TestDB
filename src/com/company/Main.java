7ae330676e0533938be1fbb1697c57d396d8bcc7
bartlomiejpogorzelski <bartlomiej.pogorzelski@onet.pl>
2017-12-12 12:26:10 +0100

    Add statement, create table in db

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -4,6 +4,7 @@ import java.net.URL;
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.SQLException;
+import java.sql.Statement;
 
 public class Main {
 
@@ -11,6 +12,10 @@ public class Main {
 	try{
 	   //Class.forName("org.sqlite.JDBC");
         Connection conn = DriverManager.getConnection("jdbc:sqlite:C:\\Users\\Bartek\\IdeaProjects\\TestDB\\testJava.db");
+        Statement statement= conn.createStatement();
+        String sql = " CREATE TABLE contacts (name TEXT, phone INTEGER, email TEXT ";
+        statement.execute(sql);
+
     }catch(SQLException e){
         System.out.println("Something went wrong " + e.getMessage());
     }
