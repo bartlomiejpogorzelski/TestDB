a0ed1c5d9ae1ec82aede5c164c861758efab9b7e
bartlomiejpogorzelski <bartlomiej.pogorzelski@onet.pl>
2017-12-12 09:14:39 +0100

    Code creating connection

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -1,8 +1,18 @@
 package com.company;
 
+import java.net.URL;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
 public class Main {
 
     public static void main(String[] args) {
-	// write your code here
+	try{
+	   //Class.forName("org.sqlite.JDBC");
+        Connection conn = DriverManager.getConnection("jdbc:sqlite:C:\\Users\\Bartek\\IdeaProjects\\TestDB\\testJava.db");
+    }catch(SQLException e){
+        System.out.println("Something went wrong " + e.getMessage());
+    }
     }
 }
