b15bf37c468f4c786487508b945e1eda3cc529da
bartlomiejpogorzelski <bartlomiej.pogorzelski@onet.pl>
2017-12-19 23:25:53 +0100

    Run program add show list with data from one table

--- src/com/company/Music/DataMusic.java
+++ src/com/company/Music/DataMusic.java
@@ -6,45 +6,44 @@ import java.util.List;
 
 public class DataMusic {
 
-    public static final String MusicDb= "music.db";
-    public static final String MusicConnection="jdbc:sqlite:C:\\Users\\Bartek\\IdeaProjects\\TestDB" + MusicDb;
+    public static final String MusicDb = "music.db";
+    public static final String MusicConnection = "jdbc:sqlite:C:\\Users\\Bartek\\IdeaProjects\\TestDB\\"+MusicDb;
 
-    public static final String SongsTable="songs";
-    public static final String SongsId="_id";
-    public static final String SongsTrack="track";
-    public static final String SongsTitle="title";
-    public static final String SongsAlbum="album";
+    public static final String SongsTable = "songs";
+    public static final String SongsId = "_id";
+    public static final String SongsTrack = "track";
+    public static final String SongsTitle = "title";
+    public static final String SongsAlbum = "album";
 
-    public static final String AlbumsTable= "albums";
-    public static final String AlbumsId="_id";
-    public static final String AlbumsName="name";
-    public static final String AlbumsArtist="artist";
+    public static final String AlbumsTable = "albums";
+    public static final String AlbumsId = "_id";
+    public static final String AlbumsName = "name";
+    public static final String AlbumsArtist = "artist";
 
-    public static final String ArtistsTable= "artists";
-    public static final String ArtistsId="_id";
-    public static final String ArtistsName="name";
+    public static final String ArtistsTable = "artists";
+    public static final String ArtistsId = "_id";
+    public static final String ArtistsName = "name";
 
-    public void connection()
-
-    {
-        try  {
-        Connection conn = DriverManager.getConnection(MusicConnection);
-        Statement statement = conn.createStatement();
-        ResultSet result;
-        result = statement.executeQuery("SELECT + FROM "+ ArtistsTable);
-
-            List list= new ArrayList();
-        while (result.next()){
-            Artists artist= new Artists();
-            artist.setId(result.getInt(ArtistsId));
-            artist.setName(result.getString(ArtistsName));
-            list.add(artist);
-        }
+    public List connection()
+        {
+            List list = new ArrayList();
+        try {
+            Connection conn = DriverManager.getConnection(MusicConnection);
+            Statement statement = conn.createStatement();
+            ResultSet result;
+            result = statement.executeQuery("SELECT * FROM "+ArtistsTable);
 
 
-    }
-    catch(SQLException e){
-        System.out.print("error  " + e.getMessage());
-    }
+            while (result.next()) {
+                Artists artist = new Artists();
+                artist.setId(result.getInt(ArtistsId));
+                artist.setName(result.getString(ArtistsName));
+                list.add(artist);
+
+            } }   catch(SQLException e){
+                System.out.print("error  " + e.getMessage());
+            }
+
+        return list;
     }
 }
--- src/com/company/Music/MainMusic.java
+++ src/com/company/Music/MainMusic.java
@@ -1,7 +1,19 @@
 package com.company.Music;
 
+import java.util.List;
+
 public class MainMusic {
-    public static final void main(String[] args){
+    public static void main(String[] args){
+
+        DataMusic dMusic = new DataMusic();
+
+        List<Artists> artists = dMusic.connection();
+
+        for( Artists artist:artists){
+            System.out.println(artist.getId()+" : "+artist.getName());
+        };
+
+
 
     }
 }
