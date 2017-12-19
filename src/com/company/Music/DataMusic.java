c196172eb8261abe5b2af95837bd554bb7e7b0a7
bartlomiejpogorzelski <bartlomiej.pogorzelski@onet.pl>
2017-12-19 12:09:18 +0100

    Add music.db and create class with this.

---
+++ src/com/company/Music/DataMusic.java
@@ -0,0 +1,25 @@
+package com.company.Music;
+
+public class DataMusic {
+
+    public static final String MusicDb= "music.db";
+    public static final String MusicConnection="C:\\Users\\Bartek\\IdeaProjects\\TestDB" + MusicDb;
+
+    public static final String SongsTable="songs";
+    public static final String SongsId="_id";
+    public static final String SongsTrack="track";
+    public static final String SongsTitle="title";
+    public static final String SongsAlbum="album";
+
+    public static final String AlbumsTable= "albums";
+    public static final String AlbumsId="_id";
+    public static final String AlbumsName="name";
+    public static final String AlbumsArtist="artist";
+
+    public static final String ArtistsTable= "artists";
+    public static final String ArtistsId="_id";
+    public static final String ArtistsName="name";
+
+
+
+}
