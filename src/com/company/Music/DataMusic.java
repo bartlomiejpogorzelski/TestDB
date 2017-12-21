1fa4f2d55b67d162068c2466ce96ea7301a2267a
bartlomiejpogorzelski <bartlomiej.pogorzelski@onet.pl>
2017-12-21 18:04:17 +0100

    Create method querryAlbumsFromArtists

--- src/com/company/Music/DataMusic.java
+++ src/com/company/Music/DataMusic.java
@@ -30,6 +30,7 @@ public class DataMusic {
     public static final int ORDER_BY_ASC = 2;
     public static final int ORDER_BY_DESC = 3;
 
+    Connection conn;
     public List connection(int number) {
         StringBuilder stringBuilder = new StringBuilder("SELECT * FROM ");
         if (number != ORDER_BY_NONE) {
@@ -45,7 +46,7 @@ public class DataMusic {
             List list = new ArrayList();
 
             try {
-                Connection conn = DriverManager.getConnection(MusicConnection);
+                conn = DriverManager.getConnection(MusicConnection);
                 Statement statement = conn.createStatement();
                 ResultSet result;
                 result = statement.executeQuery(stringBuilder.toString());
@@ -64,5 +65,60 @@ public class DataMusic {
 
             return list;
         }
-}
+
+        List<String> querryAlbumsFromArtists(String stringArtist, int orderBy){
+        StringBuilder stringBuilder =new StringBuilder("SELECT ");
+        stringBuilder.append(AlbumsTable);
+        stringBuilder.append(".");
+        stringBuilder.append(AlbumsName);
+        stringBuilder.append(" FROM ");
+        stringBuilder.append(ArtistsTable);
+        stringBuilder.append(" INNER JOIN ");
+        stringBuilder.append(AlbumsTable);
+        stringBuilder.append(" ON ");
+        stringBuilder.append(ArtistsTable);
+        stringBuilder.append(".");
+        stringBuilder.append(ArtistsId);
+        stringBuilder.append(" = ");
+        stringBuilder.append(AlbumsTable);
+        stringBuilder.append(".");
+        stringBuilder.append(AlbumsArtist);
+        stringBuilder.append(" WHERE ");
+        stringBuilder.append(ArtistsTable);
+        stringBuilder.append(".");
+        stringBuilder.append(ArtistsName);
+        stringBuilder.append(" =\"");
+        stringBuilder.append(stringArtist);
+        stringBuilder.append("\"");
+        stringBuilder.append(" ORDER BY ");
+        stringBuilder.append(AlbumsTable);
+        stringBuilder.append(".");
+        stringBuilder.append(AlbumsName);
+        stringBuilder.append(" COLLATE NOCASE ");
+        if(orderBy == ORDER_BY_ASC){
+            stringBuilder.append("ASC");
+        }
+        else{
+            stringBuilder.append("DESC");
+        }
+            List<String> albums = new ArrayList<>();
+        try{
+            conn=DriverManager.getConnection(MusicConnection);
+            Statement statement= conn.createStatement();
+            ResultSet result = statement.executeQuery(stringBuilder.toString());
+            while (result.next()) {
+            albums.add(result.getString(1));
+            }
+            return albums;
+
+        } catch (SQLException e1) {
+            System.out.print("errror2" + e1.getMessage());
+            return null;
+        }
+
+
+        }
+
+        }
+
 
